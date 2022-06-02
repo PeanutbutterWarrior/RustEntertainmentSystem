60be395f6261a5daa67a2de4c8d862f3bd0039bd
PeanutbutterWarrior <jamesandrewmcclure@yahoo.co.uk>
2022-06-02 21:27:34 +0100

    Create CPU

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,32 @@ mod processor_status;
 
 use processor_status::*;
 
+struct CPU {
+    program_counter: u16,
+    stack_pointer: u8,
+    accumulator: u8,
+    x: u8,
+    y: u8,
+    status: StatusRegister,
+}
+
+impl CPU {
+    fn new() -> Self {
+        CPU {
+            program_counter: 0,
+            stack_pointer: 0,
+            accumulator: 0,
+            x: 0,
+            y: 0,
+            status: StatusRegister::new() 
+        }
+    }
+
+    fn execute_next_instruction() {
+        todo!()
+    }
+}
+
 fn main() {
-    // let mut program_counter: u16 = 0;
-    // let mut stack_pointer: u8 = 0;
-    // let mut accumulator: u8 = 0;
-    // let mut x: u8 = 0;
-    // let mut y: u8 = 0;
-    let mut status = StatusRegister::new();
+    let cpu = CPU::new();
 }
